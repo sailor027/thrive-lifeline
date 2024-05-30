b2703581343204bda345375dcda46b9cafe89fc6
Ko <132860793+khruc-sail@users.noreply.github.com>
2024-05-30 00:01:47 -0500

    build: add instructions page

--- test_KH/CSVtoPHP/CSVtoPHP.php
+++ test_KH/CSVtoPHP/CSVtoPHP.php
@@ -2,29 +2,25 @@
 /*
 Plugin Name: CSV to PHP
 Plugin URI: https://github.com/khruc-sail/thrive-lifeline/tree/d59726f87327825c7547e7f6fae340d5a9a5359e/test_KH/CSVtoPHP
-Description: Test script to read a CSV file and display its contents in PHP
-Version: 2.0.0
+Description: Test script to read a CSV file and display its contents in PHP.
+Version: 2.1.0
 Author: Ko Horiuchi
 */
 
-
-// path to the CSV file
+// Path to the CSV file relative to this plugin directory
 $resourcesFile = plugin_dir_path(__FILE__) . 'TESTthrive_resources.csv';
-// TODO: Current filepath is relative to this plugin directory. If error, update path to local CSV file
 
-// display CSV contents
-// add_action('admin_notices', 'display_csv_contents');
+// Register the shortcode
 add_shortcode('displayResources', 'displayResourcesShortcode');
 
 function displayResourcesShortcode() {
-
     global $resourcesFile;
 
-    // open CSV file for reading
+    // Buffer output to return it properly
+    ob_start();
+
+    // Open the CSV file for reading
     if (($fileHandle = fopen($resourcesFile, 'r')) !== false) {
-        // Start output buffering to capture the table HTML
-        ob_start();
-        
         echo '<table style="border-collapse: collapse; width: 100%;">';
 
         // Read the CSV file line by line
@@ -39,13 +35,67 @@ function displayResourcesShortcode() {
 
         // Close the file handle
         fclose($fileHandle);
-
-        // Return the buffered content as a string
-        return ob_get_clean();
-
     } else {
         // Error opening the file
         return '<div class="notice notice-error is-dismissible">Error opening ' . $resourcesFile . '</div>';
     }
+
+    // Return the buffered content as a string
+    return ob_get_clean();
+}
+
+// Add a menu item to the plugin settings page
+add_action('admin_menu', 'CSVtoPHP_pluginMenu');
+
+function CSVtoPHP_pluginMenu() {
+    $hook = add_menu_page(
+        'CSV to PHP Instructions',  // Page title
+        'CSV to PHP',               // Menu title
+        'manage_options',           // Capability
+        'csv-to-php',               // Menu slug
+        'CSVtoPHP_displayInstructions', // Callback function
+        'dashicons-media-code',     // Icon URL
+        100                         // Position
+    );
+
+    add_action("load-$hook", 'csv_to_php_add_help_tab');
+}
+
+function CSVtoPHP_displayInstructions() {
+    ?>
+    <div class="wrap">
+        <h1>CSV to PHP Plugin Instructions</h1>
+        <h2>How to Use This Plugin</h2>
+        <ol>
+            <li>Ensure the CSV file <code>TESTthrive_resources.csv</code> is placed in the plugin directory: <code><?php echo plugin_dir_path(__FILE__); ?></code>.</li>
+            <li>Activate the plugin through the 'Plugins' menu in WordPress.</li>
+            <li>To display the CSV contents on a page or post, use the shortcode <code>[displayResources]</code>.</li>
+            <li>Insert the shortcode in the content area where you want the CSV contents to appear.</li>
+        </ol>
+        <h2>Example</h2>
+        <p>Edit a page or post and add the following shortcode:</p>
+        <pre><code>[displayResources]</code></pre>
+        <p>The contents of the CSV file will be displayed as a table in the location where you added the shortcode.</p>
+    </div>
+    <?php
+}
+
+function csv_to_php_add_help_tab() {
+    $screen = get_current_screen();
+    $screen->add_help_tab(array(
+        'id'      => 'csv_to_php_help_tab',
+        'title'   => 'Usage Instructions',
+        'content' => '<h2>CSV to PHP Plugin Instructions</h2>
+                        <ol>
+                            <li>Ensure the CSV file <code>TESTthrive_resources.csv</code> is placed in the plugin directory: <code>' . plugin_dir_path(__FILE__) . '</code>.</li>
+                            <li>Activate the plugin through the "Plugins" menu in WordPress.</li>
+                            <li>To display the CSV contents on a page or post, use the shortcode <code>[displayResources]</code>.</li>
+                            <li>Insert the shortcode in the content area where you want the CSV contents to appear.</li>
+                        </ol>
+                        <h2>Example</h2>
+                        <p>Edit a page or post and add the following shortcode:</p>
+                        <pre><code>[displayResources]</code></pre>
+                        <p>The contents of the CSV file will be displayed as a table in the location where you added the shortcode.</p>'
+    ));
 }
 ?>
