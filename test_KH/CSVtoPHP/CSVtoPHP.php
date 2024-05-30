44960a8cb9cb926c503622abec91185b694585ae
Ko <132860793+khruc-sail@users.noreply.github.com>
2024-05-29 23:40:18 -0500

    build: add shortcode so that CSV contents can be inserted into webpage

--- test_KH/CSVtoPHP/CSVtoPHP.php
+++ test_KH/CSVtoPHP/CSVtoPHP.php
@@ -1,48 +1,51 @@
 <?php
 /*
 Plugin Name: CSV to PHP
+Plugin URI: https://github.com/khruc-sail/thrive-lifeline/tree/d59726f87327825c7547e7f6fae340d5a9a5359e/test_KH/CSVtoPHP
 Description: Test script to read a CSV file and display its contents in PHP
-Version: 1.0.0
+Version: 2.0.0
 Author: Ko Horiuchi
 */
 
 
 // path to the CSV file
-$csvFile = plugin_dir_path(__FILE__) . 'TESTthrive_resources.csv';
+$resourcesFile = plugin_dir_path(__FILE__) . 'TESTthrive_resources.csv';
 // TODO: Current filepath is relative to this plugin directory. If error, update path to local CSV file
 
 // display CSV contents
-add_action('admin_notices', 'display_csv_contents');
+// add_action('admin_notices', 'display_csv_contents');
+add_shortcode('displayResources', 'displayResourcesShortcode');
 
-function display_csv_contents() {
-    global $csvFile;
+function displayResourcesShortcode() {
 
-    // open the CSV file for reading
-    if (($fileHandle = fopen($csvFile, 'r')) !== false) {
-        echo '<div class="notice notice-success is-dismissible">';
-        echo '<h2>CSV Contents:</h2>';
+    global $resourcesFile;
 
-        // read the CSV file line by line
-        echo '<table>';
+    // open CSV file for reading
+    if (($fileHandle = fopen($resourcesFile, 'r')) !== false) {
+        // Start output buffering to capture the table HTML
+        ob_start();
+        
+        echo '<table style="border-collapse: collapse; width: 100%;">';
+
+        // Read the CSV file line by line
         while (($row = fgetcsv($fileHandle)) !== false) {
             echo '<tr>';
             foreach ($row as $cell) {
-                echo '<td>' . htmlspecialchars($cell) . '</td>';
+                echo '<td style="border: 1px solid #ddd; padding: 8px;">' . htmlspecialchars($cell) . '</td>';
             }
             echo '</tr>';
         }
         echo '</table>';
-        echo '</div>';
 
-        // close file handle
+        // Close the file handle
         fclose($fileHandle);
 
+        // Return the buffered content as a string
+        return ob_get_clean();
+
     } else {
-        // error opening the file
-        echo '<div class="notice notice-error is-dismissible">';
-        echo "Error opening $csvFile";
-        echo '</div>';
+        // Error opening the file
+        return '<div class="notice notice-error is-dismissible">Error opening ' . $resourcesFile . '</div>';
     }
 }
-
 ?>
